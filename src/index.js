ce420ad8dd529870a80f8fbd2f042fbb493778d0
Oscar <oscar9557@hotmail.com>
2020-10-08 12:09:44 -0500

    add redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducer from "./reducers";
 
+const InitialState = {
+  cliente: {},
+  user: {},
+  fincas: [],
+  reses: [],
+  lotes: [],
+  pesajes: [],
+  registros: [],
+};
+const store = createStore(reducer, InitialState);
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+const reducer = (state, action) => {
+    //dependiendo la accion, modifica y returna el estado general de la app
+    switch (action.type) {
+      default:
+        return state;
+    }
+  };
+  
+  export default reducer;
+  
