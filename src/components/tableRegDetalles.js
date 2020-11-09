78dbbacca6007f5157895cbc96d43672aa37fce4
Oscar <oscar9557@hotmail.com>
2020-11-09 15:40:09 -0500

    styles and new data regdetail

--- src/components/tableRegDetalles.js
+++ src/components/tableRegDetalles.js
@@ -11,7 +11,7 @@ const TableRegDetalle = (props) => {
   };
   return (
     <>
-      <table id="table-to-xls" className=" table table-responsive-md TableRegDetalle-table">
+      <table id="table-to-xls" className=" table table-responsive-md">
         <thead>
           <tr>
             <th>Id</th>
--- src/pages/regDetail.js
+++ src/pages/regDetail.js
@@ -11,34 +11,71 @@ const RegDetail = (props) => {
   const { onDetail } = props;
   const [Encabezado, setEncabezado] = useState([]);
   const [Cantidades, setCantidades] = useState([]);
+  const [Totales, setTotales] = useState([]);
   let result = {};
 
   const getContenido = async () => {
     result = await getRegistro(onDetail.id);
     setEncabezado(result.encabezado);
     setCantidades(result.medidas);
+    getValores(result.medidas);
   };
 
+  const getValores = (medidas) => {
+    let promedio
+    let total=0;
+    medidas.map((peso) => {
+      console.log(peso.cantidad);
+      total += parseInt(peso.cantidad);
+    });
+    console.log(total);
+    promedio = total/medidas.length;
+    setTotales({ promedio, total });
+  };
   useEffect(() => {
     getContenido();
   }, [onDetail.id]);
+
   return (
     <div className="regDetail">
       <div className="regDetail-detailTable">
         <DetailTable data={Encabezado} />
       </div>
       <div className="regDetail-content">
-        <TableRegDetalles data={Cantidades} />
-        <div className="regDetail-optiones">
-          <ReactHTMLTableToExcel
-            id="test-table-xls-button"
-            className=" btn btn-success btn-md download-table-xls-button"
-            table="table-to-xls"
-            filename={onDetail.id}
-            sheet="pesajes"
-            buttonText="Descargar reporte"
-          />
-          <button className="btn btn-danger">Imprimir</button>
+        <div className="regDetail-content__table">
+          <TableRegDetalles data={Cantidades} />
+        </div>
+        <div className="regDetail-content__detail">
+          <div>
+            <table className="table loteDetail-data__stadistics-table">
+              <tbody>
+                <tr>
+                  <th className="loteDetail-data__thead">Total de reses: </th>
+                  <td>{Cantidades.length}</td>
+                </tr>
+                <tr>
+                  <th className="loteDetail-data__thead">Promedio de peso: </th>
+                  <td>{Totales.promedio}Kg</td>
+                </tr>
+                <tr>
+                  <th className="loteDetail-data__thead">Peso total: </th>
+                  <td>{Totales.total}Kg</td>
+                </tr>
+
+              </tbody>
+            </table>
+          </div>
+          <div className="regDetail-options">
+            <ReactHTMLTableToExcel
+              id="test-table-xls-button"
+              className=" btn btn-success btn-md download-table-xls-button"
+              table="table-to-xls"
+              filename={onDetail.id}
+              sheet="pesajes"
+              buttonText="Descargar reporte"
+            />
+            <button className="btn btn-danger">Imprimir</button>
+          </div>
         </div>
       </div>
     </div>
